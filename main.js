7736df8c141a206abdb52a6010d83fbe5dc529e4
TobyKaufman <68293413+TobyKaufman@users.noreply.github.com>
2022-10-07 23:33:17 -0400

    inclass8

---
+++ main.js
@@ -0,0 +1,15 @@
+const FRAME_HEIGHT = 200;
+const FRAME_WIDTH = 500; 
+const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
+
+const FRAME1 = d3.select("#vis1") 
+                  .append("svg") 
+                    .attr("height", FRAME_HEIGHT)   
+                    .attr("width", FRAME_WIDTH)
+                    .attr("class", "frame"); 
+
+
+d3.csv("data/data.csv").then((data) => { 
+
+
+});
